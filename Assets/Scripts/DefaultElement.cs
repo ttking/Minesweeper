66b823710cc26ffdc5acd38748fbb2057262f511
Tyrone Schumacher <tschumacher1997@gmail.com>
2017-09-24 16:11:23 +0200

    full version

---
+++ Assets/Scripts/DefaultElement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultElement : MonoBehaviour
+{
+    public bool mine;
+
+    public Sprite[] Emptytextures;
+    public Sprite Minetexture;
+
+    void Start()
+    {
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+        // randomise mines
+        mine = UnityEngine.Random.value < 0.15;
+
+    }
+
+    //load textures on click
+    public void LoadTexture(int adjacentCount)
+    {
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = Minetexture;
+        }
+        else
+            GetComponent<SpriteRenderer>().sprite = Emptytextures[adjacentCount];
+    }
+
+
+    public bool Iscovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    
+    void OnMouseUpAsButton()
+    {
+        if (mine)
+        {
+            // uncover all mines
+            Grid.uncoverMines();
+            // game over
+            print("you lose"); 
+        }
+        else
+        {
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            LoadTexture(Grid.adjacentMines(x, y));
+
+            //uncover mineless area
+            Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+
+            if (Grid.isFinished())
+                print("you win");   
+
+        }
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+
+    public static int w = 10; //width
+    public static int h = 13; //height
+    public static DefaultElement[,] elements = new DefaultElement[w, h];
+
+    //uncover all mines after hitting 1
+    public static void uncoverMines()
+    {
+        foreach (DefaultElement elem in elements)
+            if (elem.mine)
+            {
+                elem.LoadTexture(0);
+            }
+    }
+
+    // find out mine location
+    public static bool mineAt(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+            return elements[x, y].mine;
+        return false;
+    }
+
+    //count adjacent mines for an element
+    public static int adjacentMines(int x, int y)
+    {
+        int count = 0;
+
+        if (mineAt(x, y + 1)) ++count; // top
+        if (mineAt(x + 1, y + 1)) ++count; // top-right
+        if (mineAt(x + 1, y)) ++count; // right
+        if (mineAt(x + 1, y - 1)) ++count; // bottom-right
+        if (mineAt(x, y - 1)) ++count; // bottom
+        if (mineAt(x - 1, y - 1)) ++count; // bottom-left
+        if (mineAt(x - 1, y)) ++count; // left
+        if (mineAt(x - 1, y + 1)) ++count; // top-left
+
+        return count;
+    }
+
+    //flood fill empty elements
+    public static void FFuncover(int x, int y, bool[,] visited)
+    {    //coordinates in set range
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {   // visited already
+            if (visited[x, y])
+                return;
+            //uncover element
+            elements[x, y].LoadTexture(adjacentMines(x, y));
+
+            // Dont go on when close to mine
+            if (adjacentMines(x, y) > 0)
+                return;
+
+            // set flag
+            visited[x, y] = true;
+
+            FFuncover(x - 1, y, visited);
+            FFuncover(x + 1, y, visited);
+            FFuncover(x, y - 1, visited);
+            FFuncover(x, y + 1, visited);
+        }
+    }
+
+    public static bool isFinished()
+    {
+        // try to find a covered element that is not a mine
+        foreach (DefaultElement elem in elements)
+            if (elem.Iscovered() && !elem.mine)
+                return false;
+
+        // there are none so game won
+        return true;
+    }
+}
